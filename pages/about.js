c0e8d463ff1c7ca94d614aabeded437ddfbf3f3f
Lola <lmariemeehan@gmail.com>
2020-01-13 21:54:18 -0800

    Added shared MyLayout component and edited about and index files

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,12 @@
 import Header from '../components/Header';
+import Layout from '../components/MyLayout';
 
 export default function About() {
   return (
     <div>
-      <Header />
-      <p>This is the about page</p>
+      <Layout>
+        <p>This is the about page</p>
+      </Layout>
     </div>
   );
 }
