11e3ce08f93687b8457fd174ef796f4d123e15b0
Lola <lmariemeehan@gmail.com>
2020-01-14 10:09:10 -0800

    Using the router to create the dynamic page

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,15 @@ const PostLink = props => (
 	</li>
 );
 
-export default function Index() {
+export default function Blog() {
 	return (
-		<div>
 			<Layout>
-				<p>Hello Next.js</p>
+				<h1>My Blog</h1>
+				<ul>
+					<PostLink title="Hello Next.js" />
+					<PostLink title="Learn Next.js is awesome" />
+					<PostLink title="Deploy apps with Zeit" />
+				</ul>
 			</Layout>
-		</div>
 	);
 }
